327452c7aecfea1bf84e2375f6b0d607258b9974
Aman-Pal287 <palaman9786@gmail.com>
2025-05-07 17:40:08 +0530

    todo fullpage

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-var allElem = document.querySelectorAll('.elem')
-var fullElemPage = document.querySelectorAll('.fullElem')
-var fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
+function openFeatures() {
+    var allElem = document.querySelectorAll('.elem')
+    var fullElemPage = document.querySelectorAll('.fullElem')
+    var fullElemPageBackBtn = document.querySelectorAll('.fullElem .back')
 
-allElem.forEach((elem)=>{
-   elem.addEventListener('click',()=>{
-        fullElemPage[elem.id].style.display = 'block'
-   })   
-    
-})
+    allElem.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            fullElemPage[elem.id].style.display = 'block'
+        })
 
-fullElemPageBackBtn.forEach((back)=>{
-    back.addEventListener('click',()=>{
-          fullElemPage[back.id].style.display = 'none';
-    })    
-})
+    })
+
+    fullElemPageBackBtn.forEach((back) => {
+        back.addEventListener('click', () => {
+            fullElemPage[back.id].style.display = 'none';
+        })
+    })
+
+
+}
+openFeatures()
