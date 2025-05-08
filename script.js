6a0a050278501a3cf035e3c8f8c9b5525838b56a
Aman-Pal287 <palaman9786@gmail.com>
2025-05-08 15:15:51 +0530

    todo list feature

--- script.js
+++ script.js
@@ -19,3 +19,65 @@ function openFeatures() {
 
 }
 openFeatures()
+
+function todolist() {
+    let form = document.querySelector('.addTask form')
+    let taskInput = document.querySelector('.addTask form input')
+    let taskDetailsInput = document.querySelector('.addTask form textarea')
+    let taskCheckbox = document.querySelector('.addTask form #check')
+
+
+    let currentTask = []
+
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'))
+
+    } else {
+        console.log('task list is empty');
+
+    }
+
+    function renderTask() {
+        var allTask = document.querySelector('.allTask')
+        var sum = ''
+
+        currentTask.forEach((elem, indx) => {
+            sum = sum + ` <div class="task">
+              <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+              <button id=${indx}>Mark as Completed</button>
+            </div>`
+        })
+        allTask.innerHTML = sum;
+        localStorage.setItem('currentTask', JSON.stringify(currentTask))
+
+
+        document.querySelectorAll('.task button').forEach((btn) => {
+            btn.addEventListener('click', () => {
+                currentTask.splice(btn.id, 1)
+                renderTask()
+            })
+        })
+    }
+
+    renderTask();
+
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckbox.checked
+            }
+        )
+        renderTask()
+
+        taskCheckbox.checked = false;
+        taskInput.value = '';
+        taskDetailsInput = '';
+
+
+    })
+}
+todolist()
